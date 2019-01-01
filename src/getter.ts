fc3c07325cba0e7eaa9dd9b7a8f8bee47bd9d71e
Sebastian Rettig <serettig@posteo.de>
2019-01-01 23:12:42 +0100

    added command line options

--- src/getter.ts
+++ src/getter.ts
@@ -5,37 +5,51 @@ import { IRegistry } from "./iregistry";
 import LibraryMetadataGetter from "./library-metadata";
 
 export class Getter {
+  public forcedHttps = false;
+
   public constructor(private registry: IRegistry) { }
 
-  public async loadDependencies(dependencies: IDependency[], libraryDirectory: string) {
-    for (const dep of dependencies) {
-      const libraryInfo = await this.registry.getLibraryInformationForMachineName(dep.machineName);
-      if (!libraryInfo) {
-        console.error(`Could not find library metadata for ${dep.machineName}`);
-        continue;
-      }
-      const newLibraryPath = libraryDirectory + "/" + libraryInfo.devName;
-      try {
-        if ((await fs.pathExists(newLibraryPath)) === false) {
-          console.log(`Cloning ${libraryInfo.machineName} from ${libraryInfo.repository}`);
-          await git().clone(libraryInfo.repository, newLibraryPath);
-          await this.get(newLibraryPath, libraryDirectory);
-        }
-      } catch (e) {
-        console.error(`Error when cloning ${libraryInfo.machineName}.`);
-        console.error(e);
-        await fs.remove(newLibraryPath);
-      }
+  public async getDependenciesForExistingLibrary(libraryPath: string, libraryDirectory: string) {
+    const libraryMetadata = await LibraryMetadataGetter.create(libraryPath);
+    if (libraryMetadata.metadata && libraryMetadata.metadata.preloadedDependencies) {
+      await this.getDependenciesRecursive(libraryMetadata.metadata.preloadedDependencies, libraryDirectory);
+    }
+    if (libraryMetadata.metadata && libraryMetadata.metadata.editorDependencies) {
+      await this.getDependenciesRecursive(libraryMetadata.metadata.editorDependencies, libraryDirectory);
     }
   }
 
-  public async get(libraryPath: string, lilbraryDirectory: string) {
-    const libraryMetadata = await LibraryMetadataGetter.create(libraryPath);
-    if (libraryMetadata.metadata && libraryMetadata.metadata.preloadedDependencies) {
-      await this.loadDependencies(libraryMetadata.metadata.preloadedDependencies, lilbraryDirectory);
+  public async getLibraryAndDependencies(machineName: string, libraryDirectory: string) {
+    const libraryInfo = await this.registry.getLibraryInformationForMachineName(machineName);
+    if (!libraryInfo) {
+      throw new Error(`Could not find library metadata for ${machineName}`);
     }
-    if (libraryMetadata.metadata && libraryMetadata.metadata.editorDependencies) {
-      await this.loadDependencies(libraryMetadata.metadata.editorDependencies, lilbraryDirectory);
+
+    const newLibraryPath = libraryDirectory + "/" + libraryInfo.devName;
+    try {
+      if ((await fs.pathExists(newLibraryPath)) === false) {
+        let repo = libraryInfo.repository;
+        if (this.forcedHttps) {
+          repo = this.convertGitUrlToHttps(repo);
+        }
+        console.log(`Cloning ${libraryInfo.machineName} from ${repo}`);
+        await git().clone(repo, newLibraryPath);
+        await this.getDependenciesForExistingLibrary(newLibraryPath, libraryDirectory);
+      }
+    } catch (e) {
+      console.error(`Error when cloning ${libraryInfo.machineName}.`);
+      console.error(e);
+      await fs.remove(newLibraryPath);
+    }
+  }
+
+  private async getDependenciesRecursive(dependencies: IDependency[], libraryDirectory: string) {
+    for (const dep of dependencies) {
+      try {
+        await this.getLibraryAndDependencies(dep.machineName, libraryDirectory);
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
+import * as yargs from "yargs";
 import { CombinedRegistry } from "./combined-registry";
 import { Getter } from "./getter";
 import { GithubFallbackRegistry } from "./github-fallback-registry";
 import H5PRegistry from "./h5p-registry";
 
 (async () => {
+  const argv = yargs
+    .option("https", {
+      default: false,
+      describe: "use https to access GitHub (resolves authentication issues; enter any/fake credentials when asked for any!)",
+      type: "boolean"
+    })
+    .option("directory", {
+      alias: "d",
+      demand: true,
+      describe: "the directory in which the libraries will be stored",
+      type: "string"
+    })
+    .option("libpath", {
+      alias: "p",
+      describe: "the path of the directory with the library of which the dependencies should be retrieved",
+      type: "string"
+    })
+    .option("libname", {
+      alias: "n",
+      describe: "the name of library to get",
+      type: "string"
+    })
+    .argv;
+
   const h5pRegistry = await H5PRegistry.create();
   const fallbackRegistry = new GithubFallbackRegistry();
   const getter = new Getter(new CombinedRegistry(h5pRegistry, fallbackRegistry));
-  await getter.get("../h5p-course-presentation", "./libs");
+  getter.forcedHttps = argv.https;
+  if (argv.libpath) {
+    await getter.getDependenciesForExistingLibrary(argv.libpath, argv.directory);
+  }
+  if (argv.libname) {
+    await getter.getLibraryAndDependencies(argv.libname, argv.directory);
+  }
 })();
