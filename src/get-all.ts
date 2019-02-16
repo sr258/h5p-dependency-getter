eef973af729fb244272b13481299ba8ef9117ba6
Sebastian Rettig <serettig@posteo.de>
2019-02-16 11:33:18 +0100

    creation of import data working

--- src/get-all.ts
+++ src/get-all.ts
@@ -1,15 +1,86 @@
+import * as fs from "fs-extra";
+
 import { CombinedRegistry } from "./combined-registry";
+import { Getter } from "./getter";
 import { GithubFallbackRegistry } from "./github-fallback-registry";
 import H5PRegistry from "./h5p-registry";
+import { IRegistry } from "./iregistry";
+import { ILibraryData } from "./iregistry-data";
 import { LocalRegistry } from "./local-registry";
 
+async function getInternalInfo(devName, directory): Promise<any> {
+  try {
+    const libInfo = await fs.readJSON(directory + "/library.json");
+    if (!(await fs.pathExists(directory + "/language/.en.json"))) {
+      console.error(`library ${devName} has no language files.`);
+      return null;
+    }
+    return libInfo;
+  } catch (e) {
+    console.error(`No library.json in ${directory}. Skipping`);
+    return null;
+  }
+}
+
+function createWeblateImportData(lib: ILibraryData, license: string) {
+  return {
+    branch: "master",
+    file_format: "json",
+    filemask: "language/*.json",
+    license,
+    license_url: Getter.convertGitUrlToHttps(lib.repository),
+    name: lib.devName,
+    new_base: "language/.en.json",
+    push: lib.repository,
+    repo: lib.repository,
+    slug: lib.devName,
+    template: "language/.en.json",
+    vcs: "github",
+  };
+}
+
+async function addLibraryInfoRecursive(machineName: string, libraryBaseDir: string, registry: IRegistry, getter: Getter, weblateInfo: any[], checkedMachineNames: string[]) {
+  if (checkedMachineNames.includes(machineName)) {
+    return;
+  }
+
+  const info = await registry.getLibraryInformationForMachineName(machineName);
+  const libPath = await getter.getLibrary(machineName, libraryBaseDir);
+  if (info) {
+    const libInternalInfo = await getInternalInfo(info.devName, libPath);
+    if (libInternalInfo) {
+      weblateInfo.push(createWeblateImportData(info, libInternalInfo.license));
+    }
+  }
+
+  checkedMachineNames.push(machineName);
+
+  const dependencies = await getter.getDependencyInfos(libPath);
+  for (const dep of dependencies) {
+    await addLibraryInfoRecursive(dep.machineName, libraryBaseDir, registry, getter, weblateInfo, checkedMachineNames);
+  }
+}
+
 (async () => {
+  const libraryDir = "./libs";
+
   const h5pRegistry = await H5PRegistry.create();
   const fallbackRegistry = new GithubFallbackRegistry();
   const localRegistry = await LocalRegistry.create("src/registry-data.json");
   const combinedRegistry = new CombinedRegistry(h5pRegistry, localRegistry, fallbackRegistry);
 
+  const getter = new Getter(combinedRegistry);
+
   const libraries = await combinedRegistry.getAllLibraries();
-  const libraryNames = libraries.map((l) => l.devName);
-  libraryNames.forEach((n) => console.log(n));
+  const weblateInfo = [];
+  const checkedLibMachineNames = [];
+
+  for (const lib of libraries) {
+    try {
+      await addLibraryInfoRecursive(lib.machineName, libraryDir, combinedRegistry, getter, weblateInfo, checkedLibMachineNames);
+    } catch (e) {
+      continue;
+    }
+  }
+  console.log(JSON.stringify(weblateInfo));
 })();
--- src/getter.ts
+++ src/getter.ts
@@ -5,24 +5,35 @@ import { IRegistry } from "./iregistry";
 import LibraryMetadataGetter from "./library-metadata";
 
 export class Getter {
+
+  public static convertGitUrlToHttps(url: string) {
+    return url.replace(/git@github.com:(.*?)\/(.*?).git/g, "https://github.com/$1/$2");
+  }
   public forcedHttps = false;
 
   public constructor(private registry: IRegistry) { }
 
-  public async getDependenciesForExistingLibrary(libraryPath: string, libraryDirectory: string) {
+  public async getDependencyInfos(libraryPath: string): Promise<IDependency[]> {
     const libraryMetadata = await LibraryMetadataGetter.create(libraryPath);
+    let dependencies = [];
     if (libraryMetadata.metadata && libraryMetadata.metadata.preloadedDependencies) {
-      await this.getDependenciesRecursive(libraryMetadata.metadata.preloadedDependencies, libraryDirectory);
+      dependencies = dependencies.concat(libraryMetadata.metadata.preloadedDependencies);
     }
     if (libraryMetadata.metadata && libraryMetadata.metadata.editorDependencies) {
-      await this.getDependenciesRecursive(libraryMetadata.metadata.editorDependencies, libraryDirectory);
+      dependencies = dependencies.concat(libraryMetadata.metadata.editorDependencies);
     }
     if (libraryMetadata.metadata && libraryMetadata.metadata.dynamicDependencies) {
-      await this.getDependenciesRecursive(libraryMetadata.metadata.dynamicDependencies, libraryDirectory);
+      dependencies = dependencies.concat(libraryMetadata.metadata.dynamicDependencies);
     }
+    return dependencies;
   }
 
-  public async getLibraryAndDependencies(machineName: string, libraryDirectory: string) {
+  public async getDependenciesForExistingLibrary(libraryPath: string, libraryDirectory: string) {
+    const dependencies = await this.getDependencyInfos(libraryPath);
+    await this.getDependenciesRecursive(dependencies, libraryDirectory);
+  }
+
+  public async getLibrary(machineName: string, libraryDirectory: string, getDependencies: boolean = false): Promise<string> {
     const libraryInfo = await this.registry.getLibraryInformationForMachineName(machineName);
     if (!libraryInfo) {
       throw new Error(`Could not find library metadata for ${machineName}`);
@@ -33,16 +44,28 @@ export class Getter {
       if ((await fs.pathExists(newLibraryPath)) === false) {
         let repo = libraryInfo.repository;
         if (this.forcedHttps) {
-          repo = this.convertGitUrlToHttps(repo);
+          repo = Getter.convertGitUrlToHttps(repo);
         }
         console.log(`Cloning ${libraryInfo.machineName} from ${repo}`);
         await git().clone(repo, newLibraryPath);
-        await this.getDependenciesForExistingLibrary(newLibraryPath, libraryDirectory);
+        if (getDependencies) {
+          await this.getDependenciesForExistingLibrary(newLibraryPath, libraryDirectory);
+        }
       }
+      return newLibraryPath;
     } catch (e) {
       console.error(`Error when cloning ${libraryInfo.machineName}.`);
       console.error(e);
       await fs.remove(newLibraryPath);
+      throw e;
+    }
+  }
+
+  public async getLibraryAndDependencies(machineName: string, libraryDirectory: string) {
+    try {
+      await this.getLibrary(machineName, libraryDirectory, true);
+    } catch (e) {
+      return;
     }
   }
 
@@ -54,9 +77,5 @@ export class Getter {
         console.error(e);
       }
     }
-  }
-
-  private convertGitUrlToHttps(url: string) {
-    return url.replace(/git@github.com:(.*?)\/(.*?).git/g, "https://github.com/$1/$2");
   }
 }
